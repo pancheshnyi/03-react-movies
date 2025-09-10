53c4ae237adcf7338f97fa41a3b2bf5d42c6eeb6
Oleg_Pancheshniy <oleg.pancheshniy@gmail.com>
2025-09-10 13:06:28 +0300

    fix type movieService

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Movie } from "../types/movie";
+import type { Movie } from "../types/movie";
 
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
